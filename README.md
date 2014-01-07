6a581c4c5c389cc19574a74c84792ee65407c255
Tobias Alexander Franke <tob@cyberhead.de>
2014-01-08 00:00:57 +0100

    Readme + median cut

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 mediancut
 =========
 
-A basic implementation of "A Median Cut Algorithm for Light Probe Sampling"
+This is an implementation of Paul Debevec's [A Median Cut Algorithm for Light Probe Sampling](http://gl.ict.usc.edu/Research/MedianCut/). The code generates sample positions for 2^n light sources. You will still need to sum up the energy of the respective cut region to get the final flux. To run the sample, please fetch [Sean Barrett's stb_image](http://nothings.org/).
--- median_cut.cpp
+++ median_cut.cpp
@@ -1,36 +1,36 @@
 /* 
-	median_cut.cpp by Tobias Alexander Franke (tob@cyberhead.de) 2013
-	See http://www.tobias-franke.eu/?dev
+    median_cut.cpp by Tobias Alexander Franke (tob@cyberhead.de) 2013
+    See http://www.tobias-franke.eu/?dev
 
-	To get this to run, you will need stb_image.c and stb_image_write.h:
-	http://nothings.org/stb_image.c
-	http://nothings.org/stb/stb_image_write.h
+    To get this to run, you will need stb_image.c and stb_image_write.h:
+    http://nothings.org/stb_image.c
+    http://nothings.org/stb/stb_image_write.h
 
-	BSD License (http://www.opensource.org/licenses/bsd-license.php)
+    BSD License (http://www.opensource.org/licenses/bsd-license.php)
 
-	Copyright (c) 2013, Tobias Alexander Franke (tob@cyberhead.de)
-	All rights reserved.
+    Copyright (c) 2013, Tobias Alexander Franke (tob@cyberhead.de)
+    All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without modification,
-	are permitted provided that the following conditions are met:
+    Redistribution and use in source and binary forms, with or without modification,
+    are permitted provided that the following conditions are met:
 
-	* Redistributions of source code must retain the above copyright notice, this
-	  list of conditions and the following disclaimer.
+    * Redistributions of source code must retain the above copyright notice, this
+      list of conditions and the following disclaimer.
 
-	* Redistributions in binary form must reproduce the above copyright notice, this
-	  list of conditions and the following disclaimer in the documentation and/or
-	  other materials provided with the distribution.
+    * Redistributions in binary form must reproduce the above copyright notice, this
+      list of conditions and the following disclaimer in the documentation and/or
+      other materials provided with the distribution.
 
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <iostream>
